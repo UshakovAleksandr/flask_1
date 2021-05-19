1f168295e9e52d8bf4f4e454c0f38002db409b46
UshakovAleksandr <sashapimp@mail.ru>
2021-05-19 15:22:10 +0300

    add psql

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'notes.db')
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1@127.0.0.1:5432/flask_test'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SALT = 'my_sJHLHLHKLаваыпuper_s!alt_#4$4344'
